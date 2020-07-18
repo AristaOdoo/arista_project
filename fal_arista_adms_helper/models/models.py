f0e8b413fe90e25576d5538fa2eb14c8937a07e1
Randy Raharjo <randyraharjo@gmail.com>
2020-07-19 04:05:27 +0700

    [IMP] Mass APVO

--- fal_arista_adms_helper/models/models.py
+++ fal_arista_adms_helper/models/models.py
@@ -150,7 +150,7 @@ class BaseModel(models.AbstractModel):
                     # Both table have different approach
                     if m2o_model.model in ['res.partner']:
                         # Split between Customer / Vendor Transaction.
-                        if model.model in ['purchase.order', 'purchase.order.line']:
+                        if model.model in ['purchase.order', 'purchase.order.line', 'x_spkbbn']:
                             # Let's find it on business type level first, if not found, search again on company level
                             real_id = self.env[field.relation].sudo().search([('x_studio_adms_id', '=', vals[key]), (m2o_business_type.name, '=', business_type.id), (m2o_company.name, '=', business_type.company_id.id), ('supplier_rank', '>', 0)], limit=1)
                             if not real_id:
