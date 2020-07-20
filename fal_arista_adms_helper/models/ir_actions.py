9205dd2f985bd86e3b4a7bce1dfa661f8b60697a
Randy Raharjo <randyraharjo@gmail.com>
2020-07-20 21:16:43 +0700

    [IMP] Recalculate Payment needs float

--- fal_arista_adms_helper/models/ir_actions.py
+++ fal_arista_adms_helper/models/ir_actions.py
@@ -72,8 +72,8 @@ class IrActionsServer(models.Model):
             'apvo': '',
             'id_apvo_retur': 0,
             'apvo_retur': '',
-            'total_dp': 0,
-            'total_pay': 0,
+            'total_dp': 0.0,
+            'total_pay': 0.0,
         }
         if adms_id and fal_business_type:
             # Action Info
