913d85e9435619aab809edc49913e0fd0e938c45
Randy Raharjo <randyraharjo@gmail.com>
2020-07-22 11:36:33 +0700

    [IMP] Mass APVO

--- fal_arista_adms_helper/models/res_partner.py
+++ fal_arista_adms_helper/models/res_partner.py
@@ -100,30 +100,10 @@ class ResPartner(models.Model):
 
     def _get_name(self):
         """ Utility method to allow name_get to be overrided without re-browse the partner """
+        name = super(ResPartner, self)._get_name()
         partner = self
-        name = partner.name or ''
-
-        if partner.company_name or partner.parent_id:
-            if not name and partner.type in ['invoice', 'delivery', 'other']:
-                name = dict(self.fields_get(['type'])['type']['selection'])[partner.type]
-            if not partner.is_company:
-                name = self._get_contact_name(partner, name)
-        if self._context.get('show_address_only'):
-            name = partner._display_address(without_company=True)
-        if self._context.get('show_address'):
-            name = name + "\n" + partner._display_address(without_company=True)
-        name = name.replace('\n\n', '\n')
-        name = name.replace('\n\n', '\n')
-        if self._context.get('address_inline'):
-            name = name.replace('\n', ', ')
-        if self._context.get('show_email') and partner.email:
-            name = "%s <%s>" % (name, partner.email)
-        if self._context.get('html_format'):
-            name = name.replace('\n', '<br/>')
-        if self._context.get('show_vat') and partner.vat:
-            name = "%s ‒ %s" % (name, partner.vat)
         if partner.x_studio_adms_id:
-            name = "%s ‒ %s" % (name, partner.x_studio_adms_id)
+            name = "%s ‒ %s" % (partner.x_studio_adms_id, name)
         return name
 
     def name_get(self):
