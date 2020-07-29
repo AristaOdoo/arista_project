b139422e537140ac747c8c9251e8bb6d46b01380
Randy Raharjo <randyraharjo@gmail.com>
2020-07-29 14:41:55 +0700

    [FIX] Wrong Code

--- fal_arista_adms_helper/models/purchase.py
+++ fal_arista_adms_helper/models/purchase.py
@@ -8,13 +8,12 @@ class Purchase(models.Model):
     def _compute_po_value(self):
         for po in self:
             po_value = 0
-            if po.amount_untaxed:
-                if po.amount_untaxed:
-                    po_value = po.amount_untaxed
-                elif po.x_studio_amount_dpp:
-                    po_value = po.x_studio_amount_dpp
-                else:
-                    po_value = 0
+            if po.amount_untaxed > 0:
+                po_value = po.amount_untaxed
+            elif po.x_studio_amount_dpp > 0:
+                po_value = po.x_studio_amount_dpp
+            else:
+                po_value = 0
             po.fal_po_value = po_value
 
     fal_po_value = fields.Float("PO Value", compute='_compute_po_value')
