51a7326de717a647e0f2ad63a7b24a99cd3eefe4
Randy Raharjo <randyraharjo@gmail.com>
2020-10-20 17:49:52 +0700

    [FIX] No need to put all because only the py needed

--- account_invoice_fixed_discount/__manifest__.py
+++ account_invoice_fixed_discount/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2017 ForgeFlow S.L.
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 {
     "name": "Account Fixed Discount",
     "summary": "Allows to apply fixed amount discounts in invoices.",
-    "version": "13.0.1.0.2",
+    "version": "13.0.1.0.1",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-invoicing",
-    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "author": "Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
